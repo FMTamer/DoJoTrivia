acc1b3bd10f2c42b6d0421fe92d5881c8c1fb4a6
FMTamer <Frank.Mohammed.Tamer@gmail.com>
2019-01-21 02:54:16 +0000

    Automatic login after registration, logout function + page implemented and added.

--- DoJoTrivia/application.py
+++ DoJoTrivia/application.py
@@ -7,8 +7,14 @@ from tempfile import mkdtemp
 app = Flask(__name__)
 db = SQL("sqlite:///dojo.db")
 
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 def apology(message):
-    """Renders message as an apology to user."""
+    """Renders apology message on the apology.html website."""
     def escape(s):
         """
         Escape special characters.
@@ -28,6 +34,7 @@ def index():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        # Checks if the forms are filled out.
         if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirmation") or not request.form.get("emailaddress"):
             return apology("Make sure to fill out all fields!")
 
@@ -37,15 +44,32 @@ def register():
         # Executes database into result and hashes the password
         result = db.execute("INSERT INTO users (username, hash_password, email) VALUES(:username, :hash_password, :email)",
         username=request.form.get("username"), hash_password=pwd_context.hash(request.form.get("password")), email=request.form.get("emailaddress"))
-        # Checks if username already exists in database
+        # Checks if username or email is already in the database
         if not result:
             return apology("Either username or email already taken!")
 
         # query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
-        return render_template("personal-page.html")
+        # ensure username exists and password is correct
+
+        if len(rows) != 1 or not pwd_context.verify(request.form.get("password"), rows[0]["hash_password"]):
+            return apology("Invalid username and/or password!")
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["user_ID"]
+        return redirect(url_for("personal"))
     else:
         return render_template("register.html")
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return render_template("logout.html")
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
